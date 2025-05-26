d8561bc368d624170908c4c3a2f3b2e9bb08d775
MuhammadBakeri <mbakeri0612@gmail.com>
2025-05-26 21:18:21 +0800

    Update mediaPbl.js
    Menambahkan Tombol keluar dari aplikasi / website

--- mediaPbl.js
+++ mediaPbl.js
@@ -99,3 +99,45 @@ function showPage(page) {
         document.getElementById('tentang-page').classList.remove('d-none');
     }
 }
+
+// Tombol keluar dari aplikasi/website
+function logout() {
+    Swal.fire({
+        title: 'Log out',
+        text: "Anda Yakin Ingin Keluar?",
+        icon: 'question',
+        showCancelButton: true,
+        cancelButtonText: 'TIDAK',
+        confirmButtonColor: '#ff5252',
+        cancelButtonColor: '#ffe066',
+        confirmButtonText: 'Iya, keluar!',
+        customClass: {
+            popup: 'pixel-swal',
+            title: 'pixel-swal-title',
+            content: 'pixel-swal-content',
+            confirmButton: 'pixel-swal-confirm',
+            cancelButton: 'pixel-swal-cancel'
+        },
+        buttonsStyling: false,
+        backdrop: `
+            rgba(0,0,0,0.5)
+            url("data:image/svg+xml,%3Csvg width='100' height='100' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M11 18c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm48 25c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm-43-7c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm63 31c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM34 90c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm56-76c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM12 86c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm28-65c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm23-11c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-6 60c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm29 22c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zM32 63c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm57-13c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-9-21c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM60 91c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM35 41c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM12 60c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2z' fill='%23ffe066' fill-opacity='0.1' fill-rule='evenodd'/%3E%3C/svg%3E")
+        `
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: 'Berhasil Keluar!',
+                icon: 'success',
+                showConfirmButton: false,
+                timer: 1500,
+                customClass: {
+                    popup: 'pixel-swal',
+                    title: 'pixel-swal-title',
+                    icon: 'pixel-swal-icon'
+                }
+            }).then(() => {
+                window.location.href = 'Loginmediapbl.html';
+            });
+        }
+    });
+}
